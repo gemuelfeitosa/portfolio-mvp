00a6cd1a473ddc90b4b04a45ec84ded8a9ab416b
Gemuel Nunes Feitosa de Castro <gemuel.castro@insolo.com.br>
2026-04-17 22:12:16 -0300

    Remove hex e corrige CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
 import Header from './components/Header'
 import Projetos from './components/Projetos'
+import './App.css'
 
 function App() {
   return (
-    <>
+    <div style={{ color: 'black', backgroundColor: 'white', minHeight: '100vh', padding: '20px' }}>
       <Header />
       <Projetos />
-    </>
+    </div>
   )
 }
 
